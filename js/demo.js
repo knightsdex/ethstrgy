1eedf0a0c0312553e037b82d1ae6d4155fad5e78
unknown <stard8447@gmail.com>
2025-12-23 13:29:50 -0800

    .

--- js/demo.js
+++ js/demo.js
@@ -1,24 +1,7 @@
 const allLinks = document.querySelectorAll("a");
 
 function demo() {
-  const demoBlock = document.createElement("div");
-  demoBlock.innerText = "Demo version of the website.";
-  demoBlock.style.cssText = `
-  pointer-events: none;
-  user-select: none;
-  position: fixed;
-   z-index: 50;
-  bottom: 0;
-  right: 0;
-  width: auto;
-  height: auto;
-  padding: 15px;
-  color: red;
-  font-size: 21px;
-  font-weight: 700;
-   transition: opacity 0.3s ease 0s;
-}
-  `;
+
 
   document.body.append(demoBlock);
   let opacityValue = 1;
